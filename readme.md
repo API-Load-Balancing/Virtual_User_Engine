2d85226c79514bbe05831bc167f1989e8979abbc
hassan-salari <hassan_hasan_hassan@yahoo.com>
2016-05-29 15:34:12 +0430

    The First use-able version
    many changes on logic happened and this is the first working version
    based on modular structure

---
+++ readme.md
@@ -0,0 +1,16 @@
+Virtual User Engine
+=====================
+
+This is part of the "API Load Testing" project
+
+its job is to create an isolated context to run users code,  
+it does provide ability to access http requests and responses,    
+also control over users require methods with lots of administrative  
+options
+
+
+as its a project related, no more information is available,  
+if you are intrested in using it, take a look at source, or contact  
+me for more advanced questions
+
+im at: hassan.en.salari@gmail.com
---
+++ virtual_user_engine.js
@@ -0,0 +1,140 @@
+//Virtual User Engine
+'use strict';
+
+var Advance_Require = require('advance-require');
+var Http_Request_Options = require('./lib/http_request_options');
+var fs = require('fs');
+var vm = require('vm');
+var path = require('path');
+
+
+var Virtual_User_Engine = function () {
+
+    var thisObject = this;
+    this.requireOptions = new Advance_Require.options();
+    this.requestOptions = new Http_Request_Options.options();
+
+    this.timeout = 900000;
+    this.contextVariables = {};
+
+    var _Code_FileName = null;
+    var _Code_DirName = null;
+    this.setCodeFileName = function (filename) {
+        _Code_FileName = path.basename(filename);
+        _Code_DirName = path.dirname(filename);
+    };
+
+
+    var _code = "return null;";
+    this.getCodeFromFile = function (filename) {
+
+        var myRequireOptions = new Advance_Require.options();
+        myRequireOptions.addOverrideModule(filename, function (moduleName, absolutefilename, originalRequire) {
+            _code = fs.readFileSync(absolutefilename, 'utf8');
+            _Code_FileName = absolutefilename;
+            _Code_DirName = path.dirname(absolutefilename);
+            return { };
+        });
+        myRequireOptions.addPath('./node_modules');
+        var myInnerRequire = Advance_Require.getAdvanceRequire(module, myRequireOptions)
+        myInnerRequire(filename);
+    };
+    this.getCodeFromString = function (codeString) {
+        _code = codeString;
+    };
+
+
+    var wrapCode = function () {
+        return '(function (exports, require, module, __filename, __dirname) { \n' + _code + '\n});';
+    };
+
+
+
+    var upgradedModule = null;
+    var runFunction = null;
+    this.prepareToRun = function () {
+
+        var wrapedCode = wrapCode();
+        upgradedModule = Advance_Require.getUpgradedModule(module, this.requireOptions);
+        var myHttp = upgradedModule.prototype.require('http');
+        var myHttps = upgradedModule.prototype.require('https');
+        Http_Request_Options.applyRequestOptions(this.requestOptions, myHttp);
+        Http_Request_Options.applyRequestOptions(this.requestOptions, myHttps);
+
+        var context = createSafeContext();
+        var contextOptions = {
+            filename: __filename,
+            timeout: this.timeout
+        }
+        runFunction = vm.runInContext(wrapedCode, context, contextOptions);
+    } // prepare to run
+
+
+    var createSafeContext = function () {
+
+        var sandbox = {
+
+            process: process,
+            console: console,
+            setTimeout: setTimeout,
+            setInterval: setInterval,
+            setImmediate: setImmediate,
+            clearTimeout: clearTimeout,
+            clearInterval: clearInterval,
+            clearImmediate: clearImmediate
+        };
+
+        if (global.DTRACE_HTTP_SERVER_RESPONSE) {
+            sandbox.DTRACE_HTTP_SERVER_RESPONSE = global.DTRACE_HTTP_SERVER_RESPONSE;
+            sandbox.DTRACE_HTTP_SERVER_REQUEST = global.DTRACE_HTTP_SERVER_REQUEST;
+            sandbox.DTRACE_HTTP_CLIENT_RESPONSE = global.DTRACE_HTTP_CLIENT_RESPONSE;
+            sandbox.DTRACE_HTTP_CLIENT_REQUEST = global.DTRACE_HTTP_CLIENT_REQUEST;
+            sandbox.DTRACE_NET_STREAM_END = global.DTRACE_NET_STREAM_END;
+            sandbox.DTRACE_NET_SERVER_CONNECTION = global.DTRACE_NET_SERVER_CONNECTION;
+            sandbox.DTRACE_NET_SOCKET_READ = global.DTRACE_NET_SOCKET_READ;
+            sandbox.DTRACE_NET_SOCKET_WRITE = global.DTRACE_NET_SOCKET_WRITE;
+        }
+        if (global.COUNTER_NET_SERVER_CONNECTION) {
+            sandbox.COUNTER_NET_SERVER_CONNECTION = global.COUNTER_NET_SERVER_CONNECTION;
+            sandbox.COUNTER_NET_SERVER_CONNECTION_CLOSE = global.COUNTER_NET_SERVER_CONNECTION_CLOSE;
+            sandbox.COUNTER_HTTP_SERVER_REQUEST = global.COUNTER_HTTP_SERVER_REQUEST;
+            sandbox.COUNTER_HTTP_SERVER_RESPONSE = global.COUNTER_HTTP_SERVER_RESPONSE;
+            sandbox.COUNTER_HTTP_CLIENT_REQUEST = global.COUNTER_HTTP_CLIENT_REQUEST;
+            sandbox.COUNTER_HTTP_CLIENT_RESPONSE = global.COUNTER_HTTP_CLIENT_RESPONSE;
+        }
+
+        for (var item in thisObject.contextVariables) {  // Admin variables
+            sandbox[item] = thisObject.contextVariables[item];
+        }
+
+        vm.createContext(sandbox);
+        return sandbox;
+    };
+
+
+
+    this.run = function () {
+
+        if (runFunction === null) {
+            throw new Error('Code not ready to use, call "prepareToRun" first');
+        }
+        if (upgradedModule === null) {
+            throw new Error('Problem initializing module, call "prepareToRun"');
+        }
+
+        if (_Code_DirName === null || _Code_FileName === null) {
+            throw new Error('Filename of the running code not set, call "setCodeFileName"');
+        }
+        var temporaryExports = {};
+        runFunction(temporaryExports, upgradedModule.prototype.require, upgradedModule, _Code_FileName, _Code_DirName);
+    }
+
+}
+
+
+
+
+
+//---------------------------------------------------------------------------
+
+module.exports = Virtual_User_Engine;
